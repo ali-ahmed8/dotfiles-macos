cce71cd65b0486347b694f5363fdab826bd176e3
Ali Ahmed <aliahmed@Alis-iMac.local>
2026-06-11 18:23:17 +0300

    Add completion settings and <leader>p paste mapping

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -10,3 +10,22 @@ map("n", "<leader>r", ":w<CR>:!g++ % -o %< && ./%<<CR>")
 map("n", "<leader>h", ":nohlsearch<CR>")
 
 map("t", "<Esc>", [[<C-\><C-n>]])
+
+-- Show diagnostic error/warning in a floating window
+map('n', '<leader>d', vim.diagnostic.open_float, { desc = 'Show line diagnostics' })
+
+-- Paste the original copied value
+map("x", "<leader>p", '"_dP')
+
+-- Hitting Escape inside any floating popup safely closes it and returns to Insert Mode
+vim.api.nvim_create_autocmd("WinEnter", {
+  callback = function()
+    local win_config = vim.api.nvim_win_get_config(0)
+    if win_config.relative and win_config.relative ~= "" then
+      vim.keymap.set('n', '<Esc>', function()
+        vim.cmd('close')        -- 1. Close the popup window FIRST
+        vim.cmd('wincmd p')     -- 2. Jump back to your file
+      end, { buffer = true, silent = true })
+    end
+  end,
+})
--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -1,21 +1,50 @@
 return {
-  'saghen/blink.cmp',
-  lazy = false, -- Load immediately on startup
-  dependencies = 'rafamadriz/friendly-snippets',
-  version = 'v0.*',
-
-  opts = {
-    -- 'default' sets up Ctrl+n / Ctrl+p to navigate suggestions, Enter to accept
-    keymap = { preset = 'default' },
-
-    appearance = {
-      use_nvim_cmp_as_default = true,
-      nerd_font_variant = 'mono'
-    },
-
-    -- Tells blink to look for the LSP (MATLAB) suggestions
-    sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
-    },
+  "hrsh7th/nvim-cmp",
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
   },
+  config = function()
+    local cmp = require("cmp")
+
+    cmp.setup({
+      mapping = cmp.mapping.preset.insert({
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        
+        -- NEW: Scroll the documentation/arguments popup window
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),  -- Move down 4 lines
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4), -- Move up 4 lines
+
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      }),
+      sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+      }, {
+        { name = "buffer" },
+      }),
+    })
+
+    -- Keep your existing TextChangedI autocommand below this line...
+    vim.api.nvim_create_autocmd("TextChangedI", {
+      pattern = "*.m",
+      callback = function()
+        local line = vim.api.nvim_get_current_line()
+        local col = vim.api.nvim_win_get_cursor(0)[2]
+        if col > 0 then
+          local char = line:sub(col, col)
+          if char == "(" or char == "," then
+            vim.lsp.buf.signature_help()
+          end
+        end
+      end,
+    })
+  end,
 }
